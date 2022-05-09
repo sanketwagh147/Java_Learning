8389480213fd3352cebfc6740288547bf884865c
SanketWagh <firearmz0147@gmail.com>
2022-05-10 00:52:59 +0530

    initial commit till variables

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println(1010101123);
+
+        int myFirstNumber = 147;
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber;
+        mySecondNumber = 11;
+        System.out.println(mySecondNumber);
+
+        int sum;
+        sum = myFirstNumber + mySecondNumber;
+        System.out.println(myFirstNumber + mySecondNumber);
+        System.out.println(sum);
+    }
+}
