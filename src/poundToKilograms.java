f04620f6a7075f7cf8a0f08aab75913888928fbe
SanketWagh <firearmz0147@gmail.com>
2022-05-10 23:09:54 +0530

    tiil float

---
+++ src/poundToKilograms.java
@@ -0,0 +1,17 @@
+class poundToKilograms {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        double weightPounds = 200d;
+        double weightKilograms = .4535923d * weightPounds;
+
+        System.out.println(weightKilograms);
+
+        double pi = 3.1313927d;
+        double anotherNumber = 3_1.20;
+
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
+    }
+
+}
