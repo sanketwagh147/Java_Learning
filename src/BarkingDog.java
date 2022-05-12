51c559551ba2c7b046d378fa244d4c8413543285
Sanket Wagh <53272964+sanketwagh147@users.noreply.github.com>
2022-05-12 17:22:55 +0530

    completed till isBarking

---
+++ src/BarkingDog.java
@@ -0,0 +1,23 @@
+public class BarkingDog {
+
+    public static void main(String[] args) {
+    System.out.println(shouldWakeUp(true, 0));
+        
+    }
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay){
+
+        if (hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        }
+        if (hourOfDay < 8 || hourOfDay > 22){
+            if (isBarking){
+
+            return true;
+            }
+        }
+        
+    return false;
+    }
+    
+}
+
