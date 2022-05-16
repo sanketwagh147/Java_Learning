c3f42c2b6eca9f9fc62922f9097be817a204c243
SanketWagh <firearmz0147@gmail.com>
2022-05-16 20:53:29 +0530

    studying vip customers

---
+++ src/VipCustomer.java
@@ -0,0 +1,21 @@
+public class VipCustomer {
+
+    public static void main(String[] args) {
+        
+    }
+    String name;
+    String emailAddress;
+    double CreditLimit;
+    
+    public VipCustomer(){
+        this("default", "default emaail", "0.0");
+
+    }
+
+    public VipCustomer(String name, String emailAddress, string CreditLimit){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.CreditLimit = CreditLimit;
+
+    }
+}
