7a63d21d360f30fe4d3c35955953d02e642a9049
Sanket Wagh <53272964+sanketwagh147@users.noreply.github.com>
2022-05-16 12:58:13 +0530

    diagonal star

---
+++ src/DiagonalStar.java
@@ -0,0 +1,30 @@
+public class DiagonalStar {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static void printSquareStar(int number){
+        if (number < 5){
+            System.out.println("Invalid Value");
+        } else {
+            for (int row = 1; row <= number; row++){
+                for (int col = 1; col <= number; col++){
+                    if (row == 1 || row == number || col == 1 || col == number){
+                    System.out.print("*");
+                } else if (row == col) {
+                    System.out.print("*");
+                } else if (col == (number - row + 1)) {
+                    System.out.print("*");
+                } else
+                    System.out.print(" ");
+            }// close inner loop
+            System.out.println();
+
+        }
+        }
+    }
+    
+}
---
+++ src/FlourPack.java
@@ -0,0 +1,28 @@
+public class FlourPack {
+
+    public static void main(String[] args) {
+        System.out.println(canPack(1, 0, 5));
+        
+
+    }
+    
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if (bigCount < 0 || smallCount < 0 || goal < 0){
+            return false;
+
+        } else {
+
+            if ((goal) > ((bigCount *5) + smallCount)){
+                return false;
+            }
+
+            if ((goal % 5 ) > smallCount){
+                return false;
+
+            }
+        return true;
+
+            
+        }
+    }
+}
